a69985cf85133692a7a11b1a0bb6c22d9162f8a8
mikhaildev07 <93966383+mikiedev07@users.noreply.github.com>
2023-06-10 12:47:00 +0600

    feat: added static settings

--- water_level_forecast/water_level_forecast/settings.py
+++ water_level_forecast/water_level_forecast/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,19 +78,19 @@ WSGI_APPLICATION = 'water_level_forecast.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# DATABASES = {'default': dj_database_url.config(
-#     default=config("DATABASE_URL"),
-#     conn_max_age=600,
-#     conn_health_checks=True,
-# )
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {'default': dj_database_url.config(
+    default=config("DATABASE_URL"),
+    conn_max_age=600,
+    conn_health_checks=True,
+)
+}
 
 
 # Password validation
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
